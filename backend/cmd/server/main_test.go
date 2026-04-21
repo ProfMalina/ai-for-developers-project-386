113a9a3827a2883c7a2534f7d04184094b666b9a
Roman Malinovskiy <malinovskiyr@inbox.ru>
2026-04-21 22:37:01 +0300

    тестируем крон агента

--- backend/cmd/server/main_test.go
+++ backend/cmd/server/main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import "testing"
-
+// TO-DO разобраться нужен ли этой файл вообще
 func TestServerPackageHasRunnableTest(t *testing.T) {
 	// This smoke test keeps Go coverage collection working for the cmd/server
 	// package under the backend-ci workflow.
